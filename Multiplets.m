f0a47d690ae53d979c14a0198e9820f3654756f5
Johannes E. M. Mosig <jem-mosig@protonmail.com>
2017-10-12 12:05:08 +0100

    Initial commit
    new file:   .gitignore
    new file:   Kernel/init.m
    new file:   Multiplets.m

---
+++ Multiplets.m
@@ -0,0 +1,22 @@
+(* Mathematica Package *)
+(* Created by Mathematica Plugin for IntelliJ IDEA *)
+
+(* :Title: Multiplets *)
+(* :Context: Multiplets` *)
+(* :Author: jem-mosig@protonmail.com *)
+(* :Date: 2017-10-12 *)
+
+(* :Package Version: 0.1 *)
+(* :Mathematica Version: 11.1 *)
+(* :Copyright: (c) 2017 jem-mosig@protonmail.com *)
+(* :Keywords: multiplet, Young-tableaux, group theory, quantum physics *)
+(* :Discussion: *)
+
+BeginPackage["Multiplets`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+Begin["`Private`"]
+
+End[] (* `Private` *)
+
+EndPackage[]
