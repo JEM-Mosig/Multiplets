880e21b284026f1a848c5dc83cb9b69b18c18f48
Johannes E. M. Mosig <jem-mosig@protonmail.com>
2017-10-16 20:31:50 +0100

    Add usage strings
    modified:   Multiplets.m
    Add usage strings for Multiplet, MultipletQ, MultipletSum,
    MultipletProduct, MultipletDimension, $MaxMultipletPrintDimensionSU2,
    and $MaxMultipletPrintDimensionSU3.

--- Multiplets.m
+++ Multiplets.m
@@ -13,7 +13,34 @@
 (* :Discussion: *)
 
 BeginPackage["Multiplets`"]
-(* Exported symbols added here with SymbolName::usage *)
+
+If[!ValueQ[Multiplet::usage],
+    Multiplet::usage = "Multiplet[spec] is a wrapper for a general SU(n) Multiplet.";
+];
+
+If[!ValueQ[MultipletQ::usage],
+    MultipletQ::usage = "MultipletQ[expr] returns True if expr is a well defined Multiplet, and False otherwise.";
+];
+
+If[!ValueQ[MultipletSum::usage],
+    MultipletSum::usage = "MultipletSum[m1, m2] represents the sum of the multiplets m1 and m2.";
+];
+
+If[!ValueQ[MultipletProduct::usage],
+    MultipletProduct::usage = "MultipletProduct[m1, m2] represents the product of the multiplets m1 and m2.";
+];
+
+If[!ValueQ[MultipletDimension::usage],
+  MultipletDimension::usage = "MultipletDimension[m] gives the dimension of the multiplet m.";
+];
+
+If[!ValueQ[$MaxMultipletPrintDimensionSU2::usage],
+    $MaxMultipletPrintDimensionSU2::usage = "$MaxMultipletPrintDimensionSU2 is an integer which controls up to what dimension a SU(2) Multiplet is presented graphically in StandardForm.";
+];
+
+If[!ValueQ[$MaxMultipletPrintDimensionSU3::usage],
+  $MaxMultipletPrintDimensionSU3::usage = "$MaxMultipletPrintDimensionSU3 is an integer which controls up to what dimension a SU(3) Multiplet is presented graphically in StandardForm.";
+];
 
 Begin["`Private`"]
 
