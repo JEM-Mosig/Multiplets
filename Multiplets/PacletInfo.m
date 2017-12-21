a8302380eff9d63454506fe60e6cfc8441c43fc9
Johannes E. M. Mosig <jem-mosig@protonmail.com>
2017-12-21 20:24:22 +0100

    Enforce paclet structure
    renamed:    Kernel/init.m -> Multiplets/Kernel/init.m
    renamed:    Multiplets.m -> Multiplets/Multiplets.m
    new file:   Multiplets/PacletInfo.m
    renamed:    MultipletDimension.mt -> Multiplets/Testing/MultipletDimension.mt
    renamed:    MultipletQ.mt -> Multiplets/Testing/MultipletQ.mt
    renamed:    MultipletReduce.mt -> Multiplets/Testing/MultipletReduce.mt
    renamed:    Tableau.mt -> Multiplets/Testing/Tableau.mt
    renamed:    TableauAppend.mt -> Multiplets/Testing/TableauAppend.mt
    renamed:    TableauClear.mt -> Multiplets/Testing/TableauClear.mt
    renamed:    TableauDimension.mt -> Multiplets/Testing/TableauDimension.mt
    renamed:    TableauFirst.mt -> Multiplets/Testing/TableauFirst.mt
    renamed:    TableauFromMultiplet.mt -> Multiplets/Testing/TableauFromMultiplet.mt
    renamed:    TableauQ.mt -> Multiplets/Testing/TableauQ.mt
    renamed:    TableauRest.mt -> Multiplets/Testing/TableauRest.mt
    renamed:    TableauSimplify.mt -> Multiplets/Testing/TableauSimplify.mt
    renamed:    TableauToMatrix.mt -> Multiplets/Testing/TableauToMatrix.mt
    renamed:    TableauToMultiplet.mt -> Multiplets/Testing/TableauToMultiplet.mt
    renamed:    YoungTableaux.m -> Multiplets/YoungTableaux.m

---
+++ Multiplets/PacletInfo.m
@@ -0,0 +1,27 @@
+Paclet[
+  Name -> "Multiplets",
+  Version -> "0.1",
+  MathematicaVersion -> "11.0+",
+  Description -> "Defines multiplets and Young tableaux.",
+  Creator -> "Johannes E. M. Mosig",
+  Extensions -> {
+    {"Kernel", Root -> ".", Context -> "Multiplets`"},
+    {"Resource", Root -> "Testing", Resources -> {
+        "MultipletDimension.mt",
+        "MultipletQ.mt",
+        "MultipletReduce.mt",
+        "Tableau.mt",
+        "TableauAppend.mt",
+        "TableauClear.mt",
+        "TableauDimension.mt",
+        "TableauFirst.mt",
+        "TableauFromMultiplet.mt",
+        "TableauQ.mt",
+        "TableauRest.mt",
+        "TableauSimplify.mt",
+        "TableauToMatrix.mt",
+        "TableauToMultiplet.mt"
+      }
+    }
+  }
+]
